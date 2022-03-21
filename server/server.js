9df2be8c688e294bb7f32c01f59c2557e5864d44
Moshe Weiner <moshesweiner@gmail.com>
2022-03-21 20:28:42 +0200

    add mui

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app = express ();
 app.use(express.json())
 app.use(cors())
 
-app.use('/app', customersRouter)
+app.use('/api', customersRouter)
 
 app.listen(5000, () =>{
     console.log('Server started on port 5000');
